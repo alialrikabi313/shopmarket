3988b8c05b3d2d23b4e82eaa803d70c4d2ab4965
Ali Al-Rikabi <alialrikabi313a@gmail.com>
2025-08-25 00:09:12 +0300

    fix(build): bump Pillow to 11.0.0 for Python 3.13

--- core/settings/base.py
+++ core/settings/base.py
@@ -69,8 +69,12 @@ DATABASES = {
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STORAGES = {
-    "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
-    "staticfiles": {"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"},
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
 }
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
